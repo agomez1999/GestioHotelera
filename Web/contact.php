db96306b16e69062cacc285763fd41e58c6f88d0
Alex Gomez <alexalbons@hotmail.com>
2021-10-14 15:01:38 +0200

    save changes

--- Web/contact.php
+++ Web/contact.php
@@ -34,6 +34,9 @@
         <?php
             include "dades.php";
         ?>
+        <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2952.2255688067894!2d2.9624965149576226!3d42.273708048543966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ba8dd91251e3ff%3A0xe8dfb11cd9cdef78!2sInstitut%20Cendrassos!5e0!3m2!1sca!2ses!4v1634205262146!5m2!1sca!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
      </div>
 <script href="script.js"></script>
 </body>
