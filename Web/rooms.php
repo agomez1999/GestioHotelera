c255c244092130681d3bd130456f6219a5d8378e
Alex Gomez <alexalbons@hotmail.com>
2021-10-14 17:33:15 +0200

    Fixed blue button color

--- Web/rooms.php
+++ Web/rooms.php
@@ -30,12 +30,14 @@
                 </div>
                 <div class="body-border">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate varius elit quis vestibulum. Aenean at libero non neque rutrum malesuada nec eu dolor. Maecenas pulvinar velit a enim ullamcorper bibendum. Nulla nec ligula vel dui ultricies fringilla. Quisque tempor posuere diam a convallis. In sed lorem at turpis blandit fringilla. Nam tempus vitae libero faucibus maximus. Maecenas id nisi aliquet, auctor dolor non, rhoncus massa. Etiam malesuada sollicitudin elit, at bibendum quam consequat et. Sed lacus arcu, sagittis nec pharetra eget, venenatis id nibh.</p>
-                </div> 
+                <a href="#" class="a1"><span>More info</span></a>    
+            </div> 
             </div>
             <div class="p2-gray-body-row">
                 <div class="body-border">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate varius elit quis vestibulum. Aenean at libero non neque rutrum malesuada nec eu dolor. Maecenas pulvinar velit a enim ullamcorper bibendum. Nulla nec ligula vel dui ultricies fringilla. Quisque tempor posuere diam a convallis. In sed lorem at turpis blandit fringilla. Nam tempus vitae libero faucibus maximus. Maecenas id nisi aliquet, auctor dolor non, rhoncus massa. Etiam malesuada sollicitudin elit, at bibendum quam consequat et. Sed lacus arcu, sagittis nec pharetra eget, venenatis id nibh.</p>
-                </div>
+                <a href="#" class="a"><span>More info</span></a>    
+            </div>
                 <div class="body-border">
                 <img href="">
                 </div> 
