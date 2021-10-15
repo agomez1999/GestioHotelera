d2ee5d9c8b406acb19c2664c228ce0c90dae0276
Alex Gomez <alexalbons@hotmail.com>
2021-10-15 15:18:25 +0200

    idk

--- Web/rooms.php
+++ Web/rooms.php
@@ -16,7 +16,7 @@
     <div class="p2-body-container">
             <div class="p2-gray-body-row">
                 <div class="body-border">
-                    <h3>Individual room</h3>
+                    <h3 class="rooms-title">Individual room</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate varius elit quis vestibulum. Aenean at libero non neque rutrum malesuada nec eu dolor. Maecenas pulvinar velit a enim ullamcorper bibendum. Nulla nec ligula vel dui ultricies fringilla. Quisque tempor posuere diam a convallis. In sed lorem at turpis blandit fringilla. Nam tempus vitae libero faucibus maximus. Maecenas id nisi aliquet, auctor dolor non, rhoncus massa. Etiam malesuada sollicitudin elit, at bibendum quam consequat et. Sed lacus arcu, sagittis nec pharetra eget, venenatis id nibh.</p>
                     <a href="#" class="a"><span>More info</span></a>
                 </div>
@@ -29,12 +29,14 @@
                 <img href="">
                 </div>
                 <div class="body-border">
+                <h3 class="rooms-title">Double room</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate varius elit quis vestibulum. Aenean at libero non neque rutrum malesuada nec eu dolor. Maecenas pulvinar velit a enim ullamcorper bibendum. Nulla nec ligula vel dui ultricies fringilla. Quisque tempor posuere diam a convallis. In sed lorem at turpis blandit fringilla. Nam tempus vitae libero faucibus maximus. Maecenas id nisi aliquet, auctor dolor non, rhoncus massa. Etiam malesuada sollicitudin elit, at bibendum quam consequat et. Sed lacus arcu, sagittis nec pharetra eget, venenatis id nibh.</p>
                 <a href="#" class="a1"><span>More info</span></a>    
             </div> 
             </div>
             <div class="p2-gray-body-row">
                 <div class="body-border">
+                <h3 class="rooms-title">Double room with views</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate varius elit quis vestibulum. Aenean at libero non neque rutrum malesuada nec eu dolor. Maecenas pulvinar velit a enim ullamcorper bibendum. Nulla nec ligula vel dui ultricies fringilla. Quisque tempor posuere diam a convallis. In sed lorem at turpis blandit fringilla. Nam tempus vitae libero faucibus maximus. Maecenas id nisi aliquet, auctor dolor non, rhoncus massa. Etiam malesuada sollicitudin elit, at bibendum quam consequat et. Sed lacus arcu, sagittis nec pharetra eget, venenatis id nibh.</p>
                 <a href="#" class="a"><span>More info</span></a>    
             </div>
