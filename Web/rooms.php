652e7a0683a0b94fa0c776d7fd5934c5fe2d0c0e
Simon Kaufmann <super_hero_8@msn.com>
2021-10-18 19:34:53 +0200

    Slider

--- Web/rooms.php
+++ Web/rooms.php
@@ -21,9 +21,36 @@
                     <a href="#" class="a"><span>More info</span></a>
                 </div>
                 <div class="body-border">
-                <img href="">
+
+                <div class="slideshow-container">
+
+                    <!-- Full-width images with number and caption text -->
+                    <div class="mySlides fade">
+                        <div class="numbertext">1 / 3</div>
+                        <img src="Habitacions\Deluxe Room\4" style="width:100%">
+                        <div class="text">Caption Text</div>
+                    </div>
+
+                    <div class="mySlides fade">
+                        <div class="numbertext">2 / 3</div>
+                        <img src="Habitacions\Deluxe Room\5" style="width:100%">
+                        <div class="text">Caption Two</div>
+                    </div>
+
+                    <div class="mySlides fade">
+                        <div class="numbertext">3 / 3</div>
+                        <img src="Habitacions\Deluxe Room\6" style="width:100%">
+                        <div class="text">Caption Three</div>
+                    </div>
+
+                    <!-- Next and previous buttons -->
+                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                    </div>
+                    <br>
                 </div> 
             </div>
+
             <div class="p2-green-body-row">
                 <div class="body-border">
                 <img href="">
