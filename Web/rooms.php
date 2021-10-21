866974c92c3b8ad07f5c219ec2cd804338202938
Simon Kaufmann <super_hero_8@msn.com>
2021-10-21 17:16:08 +0200

    Carousel

--- Web/rooms.php
+++ Web/rooms.php
@@ -6,6 +6,10 @@
      <link href="style.css" rel="stylesheet" type="text/css">
     <link href="rooms_style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
     <?php
@@ -85,7 +89,19 @@
         while ($row = mysqli_fetch_array($result)){
             echo "<div class='p2-green-body-row'>
             <div class='body-border'>
-            <img href=''>
+            <div id='carouselExampleSlidesOnly' class='carousel slide' data-ride='carousel'>
+                <div class='carousel-inner'>
+                    <div class='carousel-item active'>
+                    <img class='d-block w-100' src=' '' alt='First slide'>
+                    </div>
+                    <div class='carousel-item active'>
+                    <img class='d-block w-100' src='...'' alt='First slide'>
+                    </div>
+                    <div class='carousel-item active'>
+                    <img class='d-block w-100' src='...'' alt='First slide'>
+                    </div>
+                </div>
+                </div>
             </div>
             <div class='body-border'>
                 <h2 class='rooms-title'>"; echo $row["tipus"]; echo "</h2>
