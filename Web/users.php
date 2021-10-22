1e74a9fda446e71eb0fe4e903c16756b7d7b5696
Alex Gomez <alexalbons@hotmail.com>
2021-10-22 15:43:15 +0200

    pa no perderlo

--- Web/users.php
+++ Web/users.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="users_style.css" type="text/css">
     <title>Hotel Trampolín | Users</title>
 </head>
 <body>
@@ -27,11 +28,12 @@
                 <td>";echo $row["id"]; echo "</td>
                 <td>";echo $row["usuari"]; echo "</td>
                 <td>";echo $row["contrasenya"]; echo "</td>
-                <td><a><i class='far fa-edit'></i></a></td>
+                <td><a id=''><i class='far fa-edit'></i></a></td>
                 <td><a><i class='far fa-trash-alt'></i></a></td>
             </tr>";
         }
     ?>
     </table>
+    <a><i class="far fa-plus-square"></i></a>
 </body>
 </html>
