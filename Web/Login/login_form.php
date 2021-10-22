5d8b8e87ad809fae10ca33f61c309d1df1a8d920
Simon Kaufmann <super_hero_8@msn.com>
2021-10-22 17:18:15 +0200

    Login/Register

--- Web/Login/login_form.php
+++ Web/Login/login_form.php
@@ -5,41 +5,15 @@
           <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
  	</head>	 
  	<body>
-        
-        <div class="login-form">
-        <table>
-        <td colspan="2" class="td">
-            <h3 class="title">Login</h3>        
-        </td>
-        <form method="post" action="login.php">
-            <tr>
-                <td>
-                    <p>Username</p>
-                </td>
-                <td>
-                    <input type="text" name="usrname">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>Password</p>
-                </td>
-                <td>
-                    <input type="password" name="passwd">
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2" class="td">
-                    <input type="submit" name="enviar" value="Login">
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2" class="td">
-                <a class="register-link" href="register_form.php">Register</a>
-                </td>
-            </tr>
+        <form class="login" method="post" action="login.php">
+        <p>Login</p>
+            <input type="text" name="usrname" placeholder="Username">
+            <input type="password" name="passwd" placeholder="Password">
+            <input type="submit" class="send" name="enviar" value="Login">
+            <a class="register-link" href="register_form.php">Register</a>
         </form>
-       </table>
-        </div>
  	</body>	 
 </html>
+
+
+
--- Web/Login/register_form.php
+++ Web/Login/register_form.php
@@ -5,35 +5,11 @@
           <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
  	</head>	 
  	<body>  
-        <div class="login-form">
-        <table>
-        <td colspan="2" class="td">
-            <h3 class="title">Register</h3>        
-        </td>
-        <form method="post" action="register.php">
-            <tr>
-                <td>
-                    <p>Username</p>
-                </td>
-                <td>
-                    <input type="text" name="usrname">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>Password</p>
-                </td>
-                <td>
-                    <input type="password" name="passwd">
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2" class="td">
-                    <input type="submit" name="enviar" value="Register">
-                </td>
-            </tr>
+     <form class="login" method="post" action="register.php">
+     <p>Register</p>
+            <input type="text" name="usrname" placeholder="Username">
+            <input type="password" name="passwd" placeholder="Password">
+            <input type="submit" class="send" name="enviar" value="Login">
         </form>
-       </table>
-        </div>
  	</body>	 
 </html>
