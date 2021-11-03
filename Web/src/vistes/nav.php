453c8255aa05d2760ebf6eeec1103dfebcda9b33
Simon Kaufmann <super_hero_8@msn.com>
2021-11-03 15:31:29 +0100

    Menu Dropdown + Crud

--- Web/src/vistes/nav.php
+++ Web/src/vistes/nav.php
@@ -1,19 +1,34 @@
 <link href="../public/styles/style.css" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <div class="header-container">
      <div>
-          <h3 class="title">Hotel Trampolín</h3>
-          <?php 
-               if (isset($_SESSION["login"])) {
-          ?>
-          <b class="login-text"><?php echo print($_SESSION["login"]["usuari"]) ?></b>
-          <a href="index.php?r=login" class="login-text" style="margin-top:25px">Cambiar sessió</a>
-          <?php if ($_SESSION["login"]["rol"] == 1) { ?>
-               <a href="index.php?r=CRUDusuaris" class="login-text" style="margin-top:40px">Taula users</a>
-          <?php } } else { ?>
-          <a class="login-text" href="index.php?r=login">Iniciar sessió</a>
-          <?php } ?>
+
+     <h3 class="title">Hotel Trampolín</h3>
+
+     <?php 
+     if (isset($_SESSION["login"])) {
+     ?>
+
+     <div class="dropdown login-text">
+          <button class="dropbtn">
+               <?php echo print($_SESSION["login"]["usuari"]) ?>
+          </button>
+          <div class="dropdown-content">
+               <a class="dropdown-item" href="index.php?r=login">Canviar Sessió</a>
+               <?php if ($_SESSION["login"]["rol"] == 1) { ?>
+                    <a class="dropdown-item" href="index.php?r=CRUDusuaris">Taula users</a>
+                    <a class="dropdown-item" href="index.php?r=CRUDhabitacions">Taula habitacions</a>
+               <?php } } else { ?>
+                    <a class="login-text" href="index.php?r=login">Iniciar sessió</a>
+                    <?php } ?>
+                    
+                    
+          </div>
+     </div>
+     
      </div>
      <div class="header-nav">
           <a id="home" href="index.php">Inici</a>
