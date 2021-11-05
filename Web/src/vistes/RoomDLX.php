0fc3d4ae7d63389dd30426fc66f2fefc93ac8a06
Simon Kaufmann <super_hero_8@msn.com>
2021-11-05 18:22:02 +0100

    Estils responsive

--- Web/src/vistes/RoomDLX.php
+++ Web/src/vistes/RoomDLX.php
@@ -10,14 +10,38 @@
 </head>
 <body>
     <?php include "nav.php" ?>
+
     <div class="grid-container">
-        <?php foreach($llistaHabitacions as $row) { ?>
-        <div>
-            <h1>Habitació <?php echo $row["nom"] ?></h1>
-            <img src="<?php echo $row["img1"] ?>" alt="">
-            <a href='#' class='a'><span>Veure habitació</span></a>  
+
+        <div class='body-border'>
+            <div id='carouselExampleIndicators<?php echo $row['id'] ?>' class='carousel slide' data-ride='carousel'>
+                <ol class='carousel-indicators'>
+                    <li data-target='#carouselExampleIndicators<?php echo $row['id'] ?>' data-slide-to='0' class='active'></li>
+                    <li data-target='#carouselExampleIndicators<?php echo $row['id'] ?>' data-slide-to='1'></li>
+                    <li data-target='#carouselExampleIndicators<?php echo $row['id'] ?>' data-slide-to='2'></li>
+                </ol>
+                <div class='carousel-inner'>
+                    <div class='carousel-item active'>
+                        <img class='d-block w-100' src='<?php echo $row['img1'] ?>' alt='First slide'>
+                    </div>
+                    <div class='carousel-item'>
+                        <img class='d-block w-100' src='<?php echo $row['img2'] ?>' alt='Second slide'>
+                    </div>
+                    <div class='carousel-item'>
+                        <img class='d-block w-100' src='<?php echo $row['img3'] ?>' alt='Third slide'>
+                    </div>
+                </div>
+                <a class='carousel-control-prev' href='#carouselExampleIndicators<?php echo $row['id'] ?>' role='button' data-slide='prev'>
+                    <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+                    <span class='sr-only'>Previous</span>
+                </a>
+                <a class='carousel-control-next' href='#carouselExampleIndicators<?php echo $row['id'] ?>' role='button' data-slide='next'>
+                    <span class='carousel-control-next-icon' aria-hidden='true'></span>
+                    <span class='sr-only'>Next</span>
+                </a>
+            </div>
         </div>
-        <?php } ?>
+        
     </div>
 <?php include "foot.php" ?>    
 </body>
