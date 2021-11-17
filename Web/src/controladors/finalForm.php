044870be86cc1c554fc284c65f7b945004b7ba48
Simon Kaufmann <super_hero_8@msn.com>
2021-11-17 15:44:56 +0100

    ijqs

---
+++ Web/src/controladors/finalForm.php
@@ -0,0 +1,14 @@
+<?php
+
+function ctrlfinalForm($peticio, $resposta, $contenidor)
+{
+    $id = $peticio->get(INPUT_GET, "id");
+    $habitacio = new \Daw\RoomsPDO($contenidor->config["db"]);
+    $llistaHabitacions = $habitacio->selectRoomByType($id);
+    $resposta->set('llistaHabitacions', $llistaHabitacions);
+
+    $resposta->SetTemplate("finalForm.php");
+    return $resposta;
+}
+
+?>
